99c2fe8b40cc6465c37aacce2b9155f52904fbb1
victorguimaraesdev <victorguimaraesmax@gmail.com>
2025-10-01 07:19:06 -0300

    Style: better Nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,21 +1,23 @@
 import Link from "next/link"
 
 export default function Nav() {
-    return(
-        <main>
-            <nav className="flex">
-                <ul>
-                    <li>
-                       <Link href={"/"}>Inicio</Link> 
-                    </li>
-                    <li>
-                        <Link href={"/sobre"}>Sobre</Link>
-                    </li>
-                    <li>
-                        <Link href={"/contato"}>Contato</Link>
-                    </li>
-                </ul>
-            </nav>
-        </main>
+    return (
+        <nav className="flex h-16 gap-4 justify-around items-center font-bold">
+            <h1 className="text-3xl">Victor<span className="text-indigo-800">Guimaraes</span></h1>
+            <ul className="flex gap-10">
+                <li>
+                    <Link href={"/"}>Inicio</Link>
+                </li>
+                <li>
+                    <Link href={"/sobre"}>Sobre</Link>
+                </li>
+                <li>
+                    <Link href={"/projetos"}>Projetos</Link>
+                </li>
+                <li>
+                    <Link href={"/contato"}>Contato</Link>
+                </li>
+            </ul>
+        </nav>
     )
 }
