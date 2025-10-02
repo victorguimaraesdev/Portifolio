81482776237440f302cf4cb8559303e540ccccbd
victorguimaraesdev <victorguimaraesmax@gmail.com>
2025-10-02 09:00:30 -0300

    Style: Tentando arrumar melhor a bordinha

--- src/components/Nav/Links.tsx
+++ src/components/Nav/Links.tsx
@@ -8,6 +8,9 @@ interface LinkProps {
 
 export default function Links({ href, children }: LinkProps) {
     return (
-        <Link href={href} className="border-b-2 border-transparent hover:border-white transition duration-1000">{children}</Link>
+        <Link href={href} className="relative group">
+            {children}
+             <span className="absolute left-0 -bottom-1 w-full h-[2px] bg-white scale-x-0 group-hover:scale-x-100 transition-transform duration-500 origin-left"></span>
+        </Link>
     );
 }
