19aba47e820738faba30df0264d3ef14712fb4a3
victorguimaraesdev <victorguimaraesmax@gmail.com>
2025-10-05 10:27:10 -0300

    Init home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1></h1>
-    </div>
+    <main className="flex w-full h-screen border border-amber-50 justify-center items-center">
+      <div className="flex flex-col w-1/2 h-9/12 border border-amber-50  ">
+        <div className="flex w-full h-1/3 border border-amber-50"></div>
+      </div>
+    </main>
   );
 }
