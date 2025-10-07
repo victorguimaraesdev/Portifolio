00341c85fb3cbb244058c5efa68c717e1579aea2
victorguimaraesdev <victorguimaraesmax@gmail.com>
2025-10-07 07:58:18 -0300

    Add Welcome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,24 @@ import { Icones } from "@/components/Home/Icones";
 
 export default function Home() {
   return (
-    <main className="flex w-full h-screen border border-amber-50 justify-center items-center">
+    <main className="flex w-full h-screen justify-center items-center">
       <div className="flex flex-col w-1/2 h-9/12 border border-amber-50  ">
         <div className="flex gap-4 w-full h-1/3 border border-amber-50 justify-center items-center">
           <Icones>
-            <img className="h-14 w-14" src="assets/incon/behance.png" alt=""/>
+            <img className="h-14 w-14" src="assets/incon/behance.png" alt="" />
           </Icones>
           <Icones>
-            <img className="h-14 w-14" src="assets/incon/github.png" alt=""/>
+            <img className="h-14 w-14" src="assets/incon/github.png" alt="" />
           </Icones>
           <Icones>
-            <img className="h-14 w-14" src="assets/incon/linkedin.png" alt=""/>
+            <img className="h-14 w-14" src="assets/incon/linkedin.png" alt="" />
           </Icones>
+        </div>
+        <div className="flex flex-col w-full h-2/3 items-center gap-2 ">
+          <h1 className="text-5xl font-bold ">Bem vindo ao meu</h1>
+          <h1 className="text-6xl font-bold ">
+            <span className="text-indigo-800">Portifolio Website!</span> 
+          </h1>
         </div>
       </div>
     </main>
--- src/components/Nav/Links.tsx
+++ src/components/Nav/Links.tsx
@@ -10,7 +10,7 @@ export default function Links({ href, children }: LinkProps) {
     return (
         <Link href={href} className="relative group">
             {children}
-             <span className="absolute left-0 -bottom-1 w-full h-[2px] bg-white scale-x-0 group-hover:scale-x-100 transition-transform duration-500 origin-left"></span>
+            <span className="absolute left-0 -bottom-1 w-full h-[2px] bg-white scale-x-0 group-hover:scale-x-100 transition-transform duration-500 origin-left"></span>
         </Link>
     );
 }
